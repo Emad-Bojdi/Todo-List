f0772f8c22c99a1ef18e30284539211fe2574d6e
Emaad <emadfredev@gmail.com>
2024-10-31 13:27:04 +0330

    Created auth folder in server side for developing singup.js file for SignUp

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,32 @@
+import connectDB from "../../../utils/connectDB"
+import User from "../../../models/User"
+import { hashPassword } from "../../../utils/auth"
+
+
+async function handler(req, res) {
+    if (req.method !== "POST") {
+        return ;
+    }
+    try {
+        await connectDB();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Error connecting to database" });
+    }
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).json({ message: "Invalid data" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(422).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = await User.create({ email, password: hashedPassword });
+    console.log(newUser)
+    return res.status(201).json({ message: "User created successfully", user: newUser });
+}
+
+export default handler
