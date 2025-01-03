4fb7f0d740481cd11f04a5fc60be6fd402085026
Emaad <emadfredev@gmail.com>
2025-01-04 00:26:54 +0330

    Changed connectDB

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,21 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    if (mongoose.connections[0].readyState) {
-      return true;
-    }
-
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected Successfully!");
-    return true;
-  } catch (error) {
-    console.error("MongoDB Connection Error:", error);
-    return false;
-  }
-};
+async function connectDB() {
+  if (mongoose.connections[0].readyState) return;
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to DB");
+}
 
 export default connectDB;
